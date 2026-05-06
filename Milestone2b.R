8f8b405602402cb6484a4b88c23d1df96a6ca81f
Alexander Williams <136854950+alexwilliams84@users.noreply.github.com>
2026-05-06 15:33:18 -0500

    Add files via upload

---
+++ Milestone2b.R
@@ -0,0 +1,43 @@
+library(ggplot2)
+library(ggfortify)
+library(tidyverse)
+library(lubridate)
+
+fedfunds <- read.csv("FEDFUNDS-2.csv")
+mortgage <- read.csv("MORTGAGE30US-2.csv")
+houses <- read.csv("HOUST1F.csv")
+
+head(fedfunds)
+head(mortgage)
+head(houses)
+
+# convert to date type
+mortgage$weeks = as.Date(mortgage$observation_date)
+
+# convert to monthly with mean
+mortgage = mortgage %>%
+  mutate(date = floor_date(weeks, "month")) %>%
+  group_by(date) %>%
+  summarise(MORTGAGE30US = mean(MORTGAGE30US, na.rm = TRUE))
+
+head(mortgage)
+
+# ts objects
+fedfunds_ts = ts(fedfunds$FEDFUNDS, start=c(1976,1), frequency=12)
+autoplot(fedfunds_ts)
+# take diff
+fedfunds_diff = diff(fedfunds_ts, differences = 1)
+# high volatility
+autoplot(fedfunds_diff)
+
+mortgage_ts = ts(mortgage$MORTGAGE30US, start=c(1976,1), frequency=12)
+autoplot(mortgage_ts)
+# take diff
+mortgage_diff = diff(mortgage_ts, differences = 1)
+autoplot(mortgage_diff)
+
+houses_ts = ts(houses$HOUST1F, start=c(1976,1), frequency=12)
+autoplot(houses_ts)
+# log diff
+houses_log_diff = diff(log(houses_ts), differences = 1)
+autoplot(houses_log_diff)
